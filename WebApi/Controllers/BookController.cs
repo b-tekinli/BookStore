386c5a2491166dccc70f33a5b8702155505b8d2d
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-09-08 16:14:29 +0300

    updated.

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.BookOperations.CreateBook;
+using WebApi.BookOperations.GetBooks;
 using WebApi.DBOperations;
+using static WebApi.BookOperations.CreateBook.CreateBookCommand;
 
-namespace WebApi.Controllers 
+namespace WebApi.Controllers
 {
     [ApiController]
     [Route("[controller]s")]
-    public class BookController : ControllerBase 
+    public class BookController : ControllerBase
     {
         private readonly BookStoreDbContext _context;
 
@@ -16,46 +19,19 @@ namespace WebApi.Controllers
         {
             _context = context;
         }
-        
-        // private static List<Book> BookList = new List<Book>() 
-        // { 
-        //     new Book { 
-        //         Id = 1,
-        //         Title = "Lean Startup", 
-        //         GenreId = 1, // Personal Growth
-        //         PageCount = 200,
-        //         PublishDate = new DateTime(2001, 04, 12)
-        //     },
 
-        //     new Book {
-        //         Id = 2,
-        //         Title = "Herland", 
-        //         GenreId = 2, // Science Fiction
-        //         PageCount = 250,
-        //         PublishDate = new DateTime(2010, 05, 23)
-        //     },
 
-        //     new Book {
-        //         Id = 3,
-        //         Title = "Dune",
-        //         GenreId = 2, // Science Fiction
-        //         PageCount = 540,
-        //         PublishDate = new DateTime(2001, 12, 21)
-        //     }
-        // };
-
-        
         [HttpGet]
-        public List<Book> GetBooks() 
+        public IActionResult GetBooks()
         {
-            var bookList = _context.Books.OrderBy(x => x.Id).ToList<Book>();
-            return bookList;
+            GetBooksQuery query = new GetBooksQuery(_context);
+            var result = query.Handle();
+            return Ok(result);
         }
-        
 
 
         [HttpGet("{id}")]
-        public Book GetById(int id) 
+        public Book GetById(int id)
         {
             var book = _context.Books.Where(book => book.Id == id).SingleOrDefault();
             return book;
@@ -72,23 +48,23 @@ namespace WebApi.Controllers
         */
 
 
-
         // Post  ==>   kendi static listemize 1 tane kitap ekleyeceğiz.
         [HttpPost]
-        public IActionResult AddBook([FromBody] Book newBook) 
+        public IActionResult AddBook([FromBody] CreateBookModel newBook)
         {
-            var book = _context.Books.SingleOrDefault(x => x.Title == newBook.Title);
-
-            if(book is not null) 
+            CreateBookCommand command = new CreateBookCommand(_context);
+            try
             {
-                return BadRequest();
+                command.Model = newBook;
+                command.Handle();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
             }
 
-            _context.Books.Add(newBook);
-            _context.SaveChanges();
             return Ok();
         }
-
 
 
         // Put   ==>   mevcut kitap üzerinde değişiklik, güncelleme yapacağız.
@@ -97,7 +73,7 @@ namespace WebApi.Controllers
         {
             var book = _context.Books.SingleOrDefault(x => x.Id == id);
 
-            if(book is null)
+            if (book is null)
             {
                 return BadRequest();
             }
@@ -117,7 +93,7 @@ namespace WebApi.Controllers
         {
             var book = _context.Books.SingleOrDefault(x => x.Id == id);
 
-            if(book is null) 
+            if (book is null)
             {
                 return BadRequest();
             }
